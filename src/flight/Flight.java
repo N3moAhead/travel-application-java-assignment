0fcd1bcd088a72d242ad239166c803f96d8ae82e
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-06-30 17:01:53 +0200

    Flight Comments

--- src/flight/Flight.java
+++ src/flight/Flight.java
@@ -7,7 +7,9 @@ import src.util.DateTimeUtils;
 public class Flight {
   private int id;
   private String flightNumber;
+  // The location from where the plain will start
   private String from;
+  // The destination for the plain
   private String to;
   private LocalDateTime departure;
   private LocalDateTime arrival;
@@ -36,6 +38,10 @@ public class Flight {
     return departure;
   }
 
+  /**
+   * Print all of the current properties to the terminal.
+   * All properties will be printed in a single line.
+   */
   public void print() {
     System.out.println(
       "ID: " + this.id
@@ -47,7 +53,18 @@ public class Flight {
     );
   }
 
+  /**
+   * Create a string that holds all current properties of the flight.
+   * @return The created search string.
+   */
   public String getSearchString() {
-    return this.flightNumber + String.valueOf(this.id);
+    return (
+      this.flightNumber
+      + String.valueOf(this.id)
+      + this.from
+      + this.to
+      + DateTimeUtils.formatDateTime(this.departure)
+      + DateTimeUtils.formatDateTime(this.arrival)
+    );
   }
 }
--- src/flight/FlightService.java
+++ src/flight/FlightService.java
@@ -12,13 +12,16 @@ import src.util.DateTimeUtils;
 import src.util.Display;
 import src.util.Form;
 
+/** Manages the creation and deletion of flights */
 public class FlightService {
   private HashMap<String, Flight> flights = new HashMap<>();
   private int id = 0;
   private Form form = new Form();
 
   public FlightService() {
+    // Load stored flights from a csv file
     List<String[]> records = CSVReader.getRecordsFromFile("./data/flights.csv");
+    // Create flight objects from the loaded flights
     for (String[] item : records) {
       this.createFlight(
         item[0],
@@ -32,6 +35,14 @@ public class FlightService {
 
   /** SETTER FUNCTIONS */
 
+  /**
+   * Creates a new Flight Object
+   * @param flightnumber The flightnumber of the flight
+   * @param from The airport the flight is going to depart from
+   * @param to The airport the flight is going to arrive at
+   * @param departure The time of the departure of the plain
+   * @param arrival The time of the arrival of the plain at the destination
+   */
   public void createFlight(String flightnumber, String from, String to, LocalDateTime departure, LocalDateTime arrival) {
     this.id += 1;
     Flight newFlight = new Flight(this.id, flightnumber, from, to, departure, arrival);
@@ -40,10 +51,18 @@ public class FlightService {
 
   /** GETTER FUNCTIONS */
 
+  /**
+   * Gets a list of flights that contain a given string inside one
+   * of there properties
+   * @param searchString A string the function should look for in the flight properties
+   * @return A list of flight objects
+   */
   public List<Flight> getFilteredFlights(String searchString) {
     List<Flight> filteredFlights = new ArrayList<>();
+    // Iterate over each flight objet
     for (Map.Entry<String, Flight> flightEntry : this.flights.entrySet()) {
       Flight currentFlight = flightEntry.getValue();
+      // Check if the properties of the current flight match the search string
       if (currentFlight.getSearchString().contains(searchString)) {
         filteredFlights.add(currentFlight);
       }
@@ -51,6 +70,11 @@ public class FlightService {
     return filteredFlights;
   }
 
+  /**
+   * Getter function to get a flight by its id
+   * @param id The id of the flight
+   * @return A flight object or null
+   */
   public Flight getFlight(int id) {
     String key = String.valueOf(id);
     if (this.flights.containsKey(key)) {
@@ -62,6 +86,10 @@ public class FlightService {
 
   /** USER MASKS */
 
+  /**
+   * A mask that will ask the user for an id of a flight
+   * @return A flight object
+   */
   public Flight flightSelection() {
     Flight flight = null;
     while (flight == null) {
@@ -69,17 +97,22 @@ public class FlightService {
       flight = this.getFlight(flightId);
     }
     return flight;
-  } 
-
+  }
+  
+  /**
+   * A mask that allows a user to search after specific flights
+   */
   public void flightSearch() {
     Display.printSubHeading("Flight-Search");
     int option = this.form.getRadioOption(
-        "Would you like to search after a specific Flight?",
-        new ArrayList<>(Arrays.asList("Yes", "No"))
-    );
+      "Would you like to search after a specific Flight?",
+      new ArrayList<>(Arrays.asList("Yes", "No"))
+      );
+      // Check if the user would like to filter all the flights
     if (option == 1) {
-      String searchString = this.form.getString("Flight-Search");
+      String searchString = this.form.getString("Search");
       List<Flight> searchedFlights = this.getFilteredFlights(searchString);
+      // If no flight could be found just print all flights
       if (searchedFlights.isEmpty()) {
         System.out.println("No Flight could be found :/ here are all flights unfiltered instead.");
         this.printFlights();
@@ -91,19 +124,27 @@ public class FlightService {
     }
   }
 
+  /**
+   * A mask that allows a user to create a new flight object
+   */
   public void flightCreator() {
     Display.printHeading("Flight Creation");
+    // Collect all the needed variables from the user
     System.out.println("Complete this form:");
     String flightNumber = this.form.getString("Flightnumber");
     String from = this.form.getString("From Airport");
     String to = this.form.getString("To Airport");
     LocalDateTime departure = this.form.getDate("Departure");
     LocalDateTime arrival = this.form.getDate("Arrival");
+    // Create a new flight object
     this.createFlight(flightNumber, from, to, departure, arrival);
   }
 
   /** DISPLAY FUNCTIONS */
 
+  /**
+   * Prints all stored flights to the terminal
+   */
   void printFlights() {
     System.out.println("\nFlights:");
     for (Map.Entry<String, Flight> flighEntry : this.flights.entrySet()) {
@@ -112,6 +153,10 @@ public class FlightService {
     }
   }
 
+  /**
+   * Prints a list of given flights to the terminal
+   * @param flights a list of flight objects
+   */
   void printFlights(List<Flight> flights) {
     System.out.println("\nFlights:");
     for (Flight flight : flights) {
